2653116df24b78d8f11d437e8d9e00c13d5d52ca
Piyush Sharma <spiyush.0212@gmail.com>
2021-03-09 19:08:26 +0530

    Final working files of game

---
+++ js/key-press.js
@@ -0,0 +1,17 @@
+// Create a centered text item at the center of the view:
+var text = new PointText({
+	point: view.center,
+	content: 'Click here to focus and then press some keys.',
+	justification: 'center',
+	fontSize: 15
+});
+
+function onKeyDown(event) {
+	// When a key is pressed, set the content of the text item:
+	text.content = 'The ' + event.key + ' key was pressed!';
+}
+
+function onKeyUp(event) {
+	// When a key is released, set the content of the text item:
+	text.content = 'The ' + event.key + ' key was released!';
+}
